e847abb91faa6ff50c5660fceccf81674c1c4c4f
Nijaz <your_email_address@example.com>
2026-02-26 12:51:36 +0300

    добавлен метод тротлинга, ограничение на количество запросов работает - "detail": "Request was throttled. Expected available in 55 seconds."

--- reference/netology_pd_diplom/netology_pd_diplom/settings.py
+++ reference/netology_pd_diplom/netology_pd_diplom/settings.py
@@ -153,6 +153,15 @@ REST_FRAMEWORK = {
 
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '2/min',
+        'user': '3/min'
+    }
+
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
